a9e7295132ebc419de5ddd51079455e8fd722342
organofcorti <andy@organofcorti.org>
2018-03-07 22:59:16 +1100

    Removed Kaiko
    Kaiko.com no longer has a public blockchain data API.

--- bitcoin-blockchain-data.R
+++ bitcoin-blockchain-data.R
@@ -17,7 +17,6 @@ invisible(lapply(libs, function(x){
 
 source("blockchain_info_func.R")
 source("blocktrail_func.R")
-source("kaiko_func.R")
 
 
 ### used by flatten_list_func function to convert to ints and convert NULL to NA
@@ -33,14 +32,13 @@ int_null_func <- function(x){
 	}
 				
 ### MY_API_KEY is the API key required by some sites (eg blocktrail.com)
-### Choose which API you want to use as args in the function
+### Choose the API you want to use as args in the function
 ### function will flatten json list into a data table
 ### 'sleep' arg uses Sys.sleep to create a pause between iterations (in seconds).
 
 flatten_list_func <- function(MY_APIKEY, API=c("blockchain.info", "blocktrail.com", "api.kaiko.com"), sleep = 0, x){
 		
 			if(API == "blockchain.info") block_data_table <- cbind(blockchain_info_func(sleep=sleep, x), API= API)
-			if(API == "api.kaiko.com") block_data_table <- cbind(kaiko_func(sleep=sleep, x), API= API)
 			if(API == "blocktrail.com") block_data_table <- cbind(blocktrail_func(MY_APIKEY=MY_APIKEY, sleep=sleep, x), API= API)
 			
 			return(block_data_table)
@@ -66,15 +64,6 @@ sleep <- 1
 #MY_APIKEY <- "<my apikey>"
 block_heights_requested <- 411300:411350
 data_dt <- rbindlist(pbsapply(block_heights_requested, function(x) flatten_list_func(MY_APIKEY=MY_APIKEY, API=API, sleep=sleep, x), simplify=F))
-csv_filename <- paste0(data_dt[1,API], "_", paste(range(data_dt$height), collapse="_"), "_data.csv")
-write.csv(data_dt, csv_filename)
-
-
-### kaiko
-API <- "api.kaiko.com"
-sleep <- 1
-blocks_requested <- 411300:411350
-data_dt <- rbindlist(pbsapply(block_heights_requested, function(x) flatten_list_func(MY_APIKEY=NULL, API=API, sleep=sleep, x), simplify=F))
 csv_filename <- paste0(data_dt[1,API], "_", paste(range(data_dt$height), collapse="_"), "_data.csv")
 write.csv(data_dt, csv_filename)
 
